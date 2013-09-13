81bb8a5bd16fa7c008657871fd5dc83e052ebe5f
Louis Miller <louis@systemli.org>
2013-09-13 14:24:23 +0200

    add dataProvider for ClientTest::testUnsupportedMethods

--- src/Tests/EtherpadLite/ClientTest.php
+++ src/Tests/EtherpadLite/ClientTest.php
@@ -2,14 +2,28 @@
 
 namespace EtherpadLite\Tests;
 
+use EtherpadLite\Client;
+
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
+    const API_KEY = 'T6UnYqgl19';
+
     /**
      * @expectedException \EtherpadLite\Exception\UnsupportedMethodException
+     * @dataProvider unsupportedMethods
      */
-    public function testUnsupportedMethods()
+    public function testUnsupportedMethods($method)
     {
-        $client = new \EtherpadLite\Client('T6UnYqgl19');
-        $client->fetchUnsupportedMethod();
+        $client = new Client(self::API_KEY);
+        $client->$method();
+    }
+
+    public function unsupportedMethods()
+    {
+        return array(
+            array('listMyImportantPad'),
+            array('getPad'),
+            array('setPadName')
+        );
     }
 }
