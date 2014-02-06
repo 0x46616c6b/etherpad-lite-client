40d1730781e70986c786187d17dc13ee0bd25a0e
louis <louis@systemli.org>
2014-02-06 21:02:38 +0100

    removed unused imports, simplify @method return Class

--- src/EtherpadLite/Client.php
+++ src/EtherpadLite/Client.php
@@ -3,55 +3,53 @@
 namespace EtherpadLite;
 
 use EtherpadLite\Exception\UnsupportedMethodException;
-use Guzzle\Http\Client as HttpClient;
-use Guzzle\Http\Exception\BadResponseException;
 
 /**
  * Class Client
  *
  * @package EtherpadLite
  *
- * @method \EtherpadLite\Response createGroup() creates a new group
- * @method \EtherpadLite\Response createGroupIfNotExistsFor($groupMapper) this functions helps you to map your application group ids to etherpad lite group ids
- * @method \EtherpadLite\Response deleteGroup($groupID) deletes a group
- * @method \EtherpadLite\Response listPads($groupID) returns all pads of this group
- * @method \EtherpadLite\Response createGroupPad($groupID, $padName, $text = null) creates a new pad in this group
- * @method \EtherpadLite\Response listAllGroups() lists all existing groups
+ * @method Response createGroup() creates a new group
+ * @method Response createGroupIfNotExistsFor($groupMapper) this functions helps you to map your application group ids to etherpad lite group ids
+ * @method Response deleteGroup($groupID) deletes a group
+ * @method Response listPads($groupID) returns all pads of this group
+ * @method Response createGroupPad($groupID, $padName, $text = null) creates a new pad in this group
+ * @method Response listAllGroups() lists all existing groups
  *
- * @method \EtherpadLite\Response createAuthor($name = null) creates a new author
- * @method \EtherpadLite\Response createAuthorIfNotExistsFor($authorMapper, $name = null) this functions helps you to map your application author ids to etherpad lite author ids
- * @method \EtherpadLite\Response listPadsOfAuthor($authorID) returns an array of all pads this author contributed to
- * @method \EtherpadLite\Response getAuthorName($authorID) Returns the Author Name of the author
+ * @method Response createAuthor($name = null) creates a new author
+ * @method Response createAuthorIfNotExistsFor($authorMapper, $name = null) this functions helps you to map your application author ids to etherpad lite author ids
+ * @method Response listPadsOfAuthor($authorID) returns an array of all pads this author contributed to
+ * @method Response getAuthorName($authorID) Returns the Author Name of the author
  *
- * @method \EtherpadLite\Response createSession($groupID, $authorID, $validUntil) creates a new session. validUntil is an unix timestamp in seconds
- * @method \EtherpadLite\Response deleteSession($sessionID) creates a new session. validUntil is an unix timestamp in seconds
- * @method \EtherpadLite\Response getSessionInfo($sessionID) returns informations about a session
- * @method \EtherpadLite\Response listSessionsOfGroup($groupID) returns all sessions of a group
- * @method \EtherpadLite\Response listSessionsOfAuthor($authorID) returns all sessions of an author
+ * @method Response createSession($groupID, $authorID, $validUntil) creates a new session. validUntil is an unix timestamp in seconds
+ * @method Response deleteSession($sessionID) creates a new session. validUntil is an unix timestamp in seconds
+ * @method Response getSessionInfo($sessionID) returns informations about a session
+ * @method Response listSessionsOfGroup($groupID) returns all sessions of a group
+ * @method Response listSessionsOfAuthor($authorID) returns all sessions of an author
  *
- * @method \EtherpadLite\Response getText($padID, $rev = null) returns the text of a pad
- * @method \EtherpadLite\Response setText($padID, $text) sets the text of a pad
- * @method \EtherpadLite\Response getHTML($padID, $rev = null) returns the text of a pad formatted as HTML
+ * @method Response getText($padID, $rev = null) returns the text of a pad
+ * @method Response setText($padID, $text) sets the text of a pad
+ * @method Response getHTML($padID, $rev = null) returns the text of a pad formatted as HTML
  *
- * @method \EtherpadLite\Response getChatHistory($padID, $start = null, $end = null) a part of the chat history, when start and end are given, the whole chat histroy, when no extra parameters are given
- * @method \EtherpadLite\Response getChatHead($padID) returns the chatHead (last number of the last chat-message) of the pad
+ * @method Response getChatHistory($padID, $start = null, $end = null) a part of the chat history, when start and end are given, the whole chat histroy, when no extra parameters are given
+ * @method Response getChatHead($padID) returns the chatHead (last number of the last chat-message) of the pad
  *
- * @method \EtherpadLite\Response createPad($padID, $text = null) creates a new (non-group) pad. Note that if you need to create a group Pad, you should call createGroupPad.
- * @method \EtherpadLite\Response getRevisionsCount($padID) returns the number of revisions of this pad
- * @method \EtherpadLite\Response padUsersCount($padID) returns the number of user that are currently editing this pad
- * @method \EtherpadLite\Response padUsers($padID) returns the list of users that are currently editing this pad
- * @method \EtherpadLite\Response deletePad($padID) deletes a pad
- * @method \EtherpadLite\Response getReadOnlyID($padID) returns the read only link of a pad
- * @method \EtherpadLite\Response setPublicStatus($padID, $publicStatus) sets a boolean for the public status of a pad
- * @method \EtherpadLite\Response getPublicStatus($padID) return true of false
- * @method \EtherpadLite\Response setPassword($padID, $password) returns ok or a error message
- * @method \EtherpadLite\Response isPasswordProtected($padID) returns true or false
- * @method \EtherpadLite\Response listAuthorsOfPad($padID) returns an array of authors who contributed to this pad
- * @method \EtherpadLite\Response getLastEdited($padID) returns the timestamp of the last revision of the pad
- * @method \EtherpadLite\Response sendClientsMessage($padID, $msg) sends a custom message of type $msg to the pad
- * @method \EtherpadLite\Response checkToken() returns ok when the current api token is valid
+ * @method Response createPad($padID, $text = null) creates a new (non-group) pad. Note that if you need to create a group Pad, you should call createGroupPad.
+ * @method Response getRevisionsCount($padID) returns the number of revisions of this pad
+ * @method Response padUsersCount($padID) returns the number of user that are currently editing this pad
+ * @method Response padUsers($padID) returns the list of users that are currently editing this pad
+ * @method Response deletePad($padID) deletes a pad
+ * @method Response getReadOnlyID($padID) returns the read only link of a pad
+ * @method Response setPublicStatus($padID, $publicStatus) sets a boolean for the public status of a pad
+ * @method Response getPublicStatus($padID) return true of false
+ * @method Response setPassword($padID, $password) returns ok or a error message
+ * @method Response isPasswordProtected($padID) returns true or false
+ * @method Response listAuthorsOfPad($padID) returns an array of authors who contributed to this pad
+ * @method Response getLastEdited($padID) returns the timestamp of the last revision of the pad
+ * @method Response sendClientsMessage($padID, $msg) sends a custom message of type $msg to the pad
+ * @method Response checkToken() returns ok when the current api token is valid
  *
- * @method \EtherpadLite\Response listAllPads() lists all pads on this epl instance
+ * @method Response listAllPads() lists all pads on this epl instance
  *
  */
 class Client
