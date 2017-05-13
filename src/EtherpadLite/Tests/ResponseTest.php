ad0d37063cdea52ed91a9ed9e185b647f4d70399
louis <louis@systemli.org>
2017-05-13 17:26:38 +0200

    added test for Response::getData()

--- src/EtherpadLite/Tests/ResponseTest.php
+++ src/EtherpadLite/Tests/ResponseTest.php
@@ -2,6 +2,9 @@
 
 namespace EtherpadLite\Tests;
 
+use EtherpadLite\Response;
+use Psr\Http\Message\ResponseInterface;
+
 class ResponseTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -9,21 +12,7 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
      */
     public function testResponse($rawResponse, $expected)
     {
-        $httpResponse = $this->getMockBuilder('\Psr\Http\Message\ResponseInterface')
-            ->setConstructorArgs([200, null, $rawResponse])
-            ->getMock();
-
-        $httpResponse
-            ->expects($this->once())
-            ->method('getStatusCode')
-            ->will($this->returnValue(200));
-
-        $httpResponse
-            ->expects($this->once())
-            ->method('getBody')
-            ->will($this->returnValue($rawResponse));
-
-        $response = new \EtherpadLite\Response($httpResponse);
+        $response = $this->getMockedResponse($rawResponse);
 
         $this->assertNotEmpty($response->getResponse());
         $this->assertArrayHasKey('code', $response->getResponse());
@@ -43,9 +32,9 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
                     'code' => 0,
                     'message' => 'ok',
                     'data' => [
-                        'groupID' => 'g.s8oes9dhwrvt0zif'
-                    ]
-                ]
+                        'groupID' => 'g.s8oes9dhwrvt0zif',
+                    ],
+                ],
             ],
             [
                 '{"code":0,"message":"ok","data":{"padIDs":["g.OTeVWJ4KuZyRBlDI$aaa","meinNEUESPad","sjImkoFqDK","t08dXmHh1t"]}}',
@@ -53,26 +42,61 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
                     'code' => 0,
                     'message' => 'ok',
                     'data' => [
-                        'padIDs' => ['g.OTeVWJ4KuZyRBlDI$aaa','meinNEUESPad','sjImkoFqDK','t08dXmHh1t']
-                    ]
-                ]
+                        'padIDs' => ['g.OTeVWJ4KuZyRBlDI$aaa', 'meinNEUESPad', 'sjImkoFqDK', 't08dXmHh1t'],
+                    ],
+                ],
             ],
             [
                 '{"code":4,"message":"no or wrong API Key","data":null}',
                 [
                     'code' => 4,
                     'message' => 'no or wrong API Key',
-                    'data' => null
-                ]
+                    'data' => null,
+                ],
             ],
             [
                 '{"code":0,"message":"ok","data":null}',
                 [
                     'code' => 0,
                     'message' => 'ok',
-                    'data' => null
-                ]
+                    'data' => null,
+                ],
             ],
         ];
+    }
+
+    public function testGetData()
+    {
+        $rawResponse = '{"code":0,"message":"ok","data":{"groupID": "g.s8oes9dhwrvt0zif"}}';
+        $response = $this->getMockedResponse($rawResponse);
+
+        $this->assertEquals('g.s8oes9dhwrvt0zif', $response->getData('groupID'));
+        $this->assertNull($response->getData('padID'));
+    }
+
+    /**
+     * @param $rawResponse
+     * @return Response
+     */
+    private function getMockedResponse($rawResponse)
+    {
+        $httpResponse = $this->getMockBuilder('\Psr\Http\Message\ResponseInterface')
+            ->setConstructorArgs([200, null, $rawResponse])
+            ->getMock();
+
+        $httpResponse
+            ->expects($this->once())
+            ->method('getStatusCode')
+            ->will($this->returnValue(200));
+
+        $httpResponse
+            ->expects($this->once())
+            ->method('getBody')
+            ->will($this->returnValue($rawResponse));
+
+        /** @var ResponseInterface $httpResponse */
+        $response = new Response($httpResponse);
+
+        return $response;
     }
 }
